70ba480d67697ea73eebf7a0f980fc47d542327d
zsantana <reinaldojsantana@gmail.com>
2023-09-11 08:45:22 -0300

    Ajuste no start do postgres e script DDL

--- ddl.sql
+++ ddl.sql
@@ -3,7 +3,7 @@ SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
+SELECT pg_catalog.set_config('search_path', 'public', false); -- Defina o esquema como 'public' aqui
 SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
@@ -15,22 +15,26 @@ SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
-DROP TABLE IF EXISTS PUBLIC."PESSOAS";
+DROP TABLE IF EXISTS public.PESSOAS;
 
-CREATE TABLE PUBLIC."PESSOAS" (
-    "ID" uuid not null,
-    "APELIDO" varchar(32) unique not null,
-    "NASCIMENTO" varchar(12) not null,
-    "NOME" varchar(100) not null,
-    "STACK" varchar(255),
-    primary key ("ID"),
-    ,BUSCA_TRGM TEXT GENERATED ALWAYS AS (
+CREATE TABLE public.PESSOAS (
+    ID uuid NOT NULL,
+    APELIDO VARCHAR(32) UNIQUE NOT NULL,
+    NASCIMENTO VARCHAR(12) NOT NULL,
+    NOME VARCHAR(100) NOT NULL,
+    STACK VARCHAR(255),
+    PRIMARY KEY (ID),
+    BUSCA_TRGM TEXT GENERATED ALWAYS AS (
         NOME || ' ' || APELIDO || ' ' || COALESCE(STACK, '')
-    ) STORED not null
+    ) STORED NOT NULL
 );
 
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS IDX_PESSOAS_BUSCA_TGRM ON PESSOA USING GIST (BUSCA_TRGM GIST_TRGM_OPS(siglen=256)) 
-INCLUDE(apelido, nascimento, nome, publicID, stack);
+-- Defina o esquema 'public' novamente antes de criar o índice
+SELECT pg_catalog.set_config('search_path', 'public', false);
 
-ALTER TABLE PUBLIC."PESSOAS" OWNER TO rinha;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS IDX_PESSOAS_BUSCA_TGRM ON public.PESSOAS USING GIST (BUSCA_TRGM GIST_TRGM_OPS(siglen=256)) 
+INCLUDE(apelido, nascimento, nome, ID, stack);
+
+ALTER TABLE public.PESSOAS OWNER TO rinha;
